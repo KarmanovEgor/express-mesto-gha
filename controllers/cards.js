8bc0d1cdd1342ce38b3d29dab091742f765778dc
Karmanov Egor <karmanov.video@yandex.ru>
2023-09-11 16:19:51 +0700

    fix rev1 test3

--- controllers/cards.js
+++ controllers/cards.js
@@ -27,35 +27,30 @@ module.exports.getCards = (req, res, next) => {
     .then((cards) => res.status(HTTP_STATUS_OK).send(cards))
     .catch(next);
 };
+
 module.exports.deleteCard = (req, res, next) => {
   Card.findById(req.params.cardId)
     .then((card) => {
-      if (!card.owner.equals(req.user._id)) {
-        throw new ForbiddenError('бро, чужая карточка');
+      if (!card) {
+        throw new NotFoundError(`Карточка с идентификатором ${req.params.cardId} не найдена`);
       }
-      Card.deleteOne(card)
-        .orFail()
-        .then(() => {
-          res.status(HTTP_STATUS_OK).send({ message: 'Карточка удалена' });
-        })
-        .catch((err) => {
-          if (err instanceof mongoose.Error.DocumentNotFoundError) {
-            next(new NotFoundError(`карточка с данным идентификатором не найдена ${req.params.cardId}`));
-          } else if (err instanceof mongoose.Error.CastError) {
-            next(new BadRequestError(`Некорректный идентификатор карточки: ${req.params.cardId}`));
-          } else {
-            next(err);
-          }
-        });
+      if (!card.owner.equals(req.user._id)) {
+        throw new ForbiddenError('Нельзя удалить чужую карточку');
+      }
+      return Card.deleteOne({ _id: card._id });
+    })
+    .then(() => {
+      res.status(HTTP_STATUS_OK).send({ message: 'Карточка удалена' });
     })
     .catch((err) => {
-      if (err instanceof mongoose.Error.DocumentNotFoundError) {
-        next(new NotFoundError(`карточка с данным идентификатором не найдена ${req.params.cardId}`));
-      } else {
+      if (err instanceof mongoose.Error.CastError || err instanceof ForbiddenError) {
+        next(new BadRequestError(err.message));
+      } else if (err instanceof NotFoundError) {
         next(err);
       }
     });
 };
+
 module.exports.likeCard = (req, res, next) => {
   Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
     .orFail()
