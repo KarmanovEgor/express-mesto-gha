27d4f7e47181be13002da8ed4e1c237d4c03ad94
Karmanov Egor <karmanov.video@yandex.ru>
2023-09-11 11:05:17 +0700

    Feat: backend full, test4

--- routes/signup.js
+++ routes/signup.js
@@ -7,7 +7,7 @@ const { createUser } = require('../controllers/users');
 
 router.post('/', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().email(),
+    email: Joi.string().required().email(),
     password: Joi.string().required().min(2),
     name: Joi.string().min(2).max(30),
     // avatar: Joi.string().allow('').pattern(new RegExp(validator.isURL())),
